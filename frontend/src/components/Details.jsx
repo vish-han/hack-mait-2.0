50356831900ac79dbf83779e8dd6ccc0662bb676
anonymous1508 <skscool95@gmail.com>
2022-12-22 15:38:43 +0530

    detailsboxadded

--- frontend/src/components/Details.jsx
+++ frontend/src/components/Details.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 const Details = () => {
   return (
     <div>
+      <div className='data flex box-border w-[1110px] h-[340px] m-auto  bg-black rounded-3xl cursor-pointer justify-evenly shadow-2xl shadow-purple-500 hover:shadow-purple-700' >
+        <div classname='topbox  top-[40px] '></div>
+        <div classname='image '>image</div>
+      </div>
         
     </div>
   )
